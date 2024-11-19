19478cdcb57ab3a2adcbbe53dac4b1f49ab5c6c4
Dorian <adrianheras@profesorestrazos.net>
2024-11-19 18:08:07 +0100

    simplify

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,7 @@
 const accordionElement = document.getElementById('accordion');
-const allAccordionTexts = document.querySelectorAll('.accordion-text');
 
 const toggleAccordionText = event => {
-  console.log(event.target);
-  // for (const accordionText of allAccordionTexts) {
-  //   accordionText.classList.remove('accordion-text-show');
-  // }
-
-  event.target.children[0].children[1].classList.toggle('accordion-text-show');
+  event.target.classList.toggle('accordion-item-show');
 };
 
 accordionElement.addEventListener('click', toggleAccordionText);
